8e6702c82ad2346f8e368600cd72e81a579abf03
elizabethra16 <elizabeth160102@gmail.com>
2021-11-15 17:35:28 +0700

    first commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    use ApiResponse;
+}
+public function login(LoginRequest $request){
+    $validated = $request->validated();
+
+    if(!Auth::attempt($validated)){
+        return $this->apiError('Credentials not match', Response::HTTP_UNAUTHORIZED);
+    }
+
+    $user = User::where('email', $validated['email'])->first();
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return $this->apiSucces([
+        'token' => $token,
+        'token_type' => 'Bearer',
+        'user' => $user,
+    ]);
+}
